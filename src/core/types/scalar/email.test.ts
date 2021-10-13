aee86f38bca012b4fdc7cdcf6d28fe0baa659813
Tarcisio Corte <tarcisiocorte@gmail.com>
2021-10-13 16:13:45 -0300

    add tests for email and url codecs

--- src/core/types/scalar/email.test.ts
+++ src/core/types/scalar/email.test.ts
@@ -1,12 +1,12 @@
-import { emailCodec } from './email'
-import * as E from 'fp-ts/Either'
 import { pipe } from 'fp-ts/function'
+import { mapAllE } from '@/config/tests/fixtures'
+import { emailCodec } from './email'
 
 it('Deveria validar o email corretamente', () => {
   pipe(
     'john@doe.com',
     emailCodec.decode,
-    E.map(result => expect(result).toBe('john@doe.com')),
+    mapAllE(result => expect(result).toBe('john@doe.com')),
   )
 })
 
@@ -14,6 +14,9 @@ it('Deveria retornar um erro quando o email for inválido', () => {
   pipe(
     'invalid-email',
     emailCodec.decode,
-    E.mapLeft(error => expect(error[0]?.message).toBe('Invalid email')),
+    mapAllE(error => {
+      const errorMessage: string = Array.isArray(error) ? error[0]?.message : ''
+      expect(errorMessage).toBe('Invalid email')
+    }),
   )
 })
